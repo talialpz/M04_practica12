e4a7aa469f6a2da60efc42f2adde90108f8ce496
Talía <tlopez22@jaumebalmes.net>
2023-02-15 20:47:33 +0100

    Practica 12

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ print(imprimirJson.imprimir())
 
 import exercici1_B as copiarJson
 print(copiarJson.copiar())
+
+#S'importa l'exercici 1_A al main per tal de mostrar la nostra funció de l'arxiu XML.
+import Exercici1_A as fitxerXml
+print(fitxerXml.ArxiuXML())
