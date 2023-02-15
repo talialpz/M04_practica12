2427ec3d5bee1bef7ee3a299e54cbb37cee938c2
Ramon <rfont22@jaumebalmes.net>
2023-02-15 20:45:27 +0100

    Practica12

---
+++ main.py
@@ -0,0 +1,7 @@
+#Aqui fem els imports per trucar a les funcions
+import exercici1_B as imprimirJson
+print(imprimirJson.imprimir())
+#Aqui fem els imports per trucar a les funcions
+
+import exercici1_B as copiarJson
+print(copiarJson.copiar())
